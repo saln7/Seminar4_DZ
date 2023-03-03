7c86daf0288c3196c31cb08cdaa90414d8599e00
OH_WOW <s.a_ln@mail.ru>
2023-03-03 16:23:14 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+// Console.WriteLine("введите число A");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("ведите число B");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int step = a;
+// for (int i=1; i < b; i++);
+// {
+//     step = step * a;
+// }
+// Console.WriteLine("A в степени B равно:" + step);
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+// 
+// Console.WriteLine("введите число");
+// int i = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+
+// while (i > 0)
+// {
+// int num = i % 10;
+// i = i / 10;
+// sum = sum + num;
+// }
+// Console.WriteLine("сумма всех цифр в числе равна: " + sum);
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+// int[] ArrayInit(int enterNum)
+// {
+//     int[] array=new int[enterNum]; 
+//     for (int i=0;i<enterNum;i++)
+// {
+//  array[i]=new Random().Next(0,30);
+//  }
+//  return array;
+//  }
+
+//  Console.Write("Введите число, для задания массива: ");
+//  int enterNum=Convert.ToInt32(Console.ReadLine());
+//  int[] result=ArrayInit(enterNum);
